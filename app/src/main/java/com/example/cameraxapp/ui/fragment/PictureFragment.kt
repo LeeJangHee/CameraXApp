d9ab05e5fe686cf6ca616cce041ed5bc74e6b883
DESKTOP-D6FEIOL\wkdgm <sj90947@fixelsoft.com>
2021-04-20 14:13:43 +0900

    [FIX] startActivityForResult()의 requestCode 값을 Activity 값을 사용하지 말자

--- app/src/main/java/com/example/cameraxapp/ui/fragment/PictureFragment.kt
+++ app/src/main/java/com/example/cameraxapp/ui/fragment/PictureFragment.kt
@@ -1,20 +1,33 @@
 package com.example.cameraxapp.ui.fragment
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.cameraxapp.R
 import com.example.cameraxapp.ui.activity.CameraActivity
+import com.example.cameraxapp.ui.activity.MainActivity
+import com.example.cameraxapp.util.Constants.Companion.ALBUM_OK
+import com.example.cameraxapp.util.Constants.Companion.PERMISSION_CAMERA
+import com.example.cameraxapp.util.Constants.Companion.TAG
 import com.example.cameraxapp.util.PermissionCheck
 import kotlinx.android.synthetic.main.fragment_picture.view.*
 
 
 class PictureFragment : Fragment() {
 
+    lateinit var mainActivity: MainActivity
     lateinit var permissionCheck: PermissionCheck
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = MainActivity()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,14 +41,17 @@ class PictureFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         permissionCheck = PermissionCheck(this@PictureFragment, object : PermissionCheck.PermissionListener {
             override fun permissionAllowed() {
-                val intent = Intent(requireActivity(), CameraActivity::class.java)
-                startActivity(intent)
+//                mainActivity.openCameraActivity()
+                val intent = Intent(activity, CameraActivity::class.java)
+
+                // requestCode 값으로 안드로이드 값을 넣으면 안된다.
+                activity?.startActivityForResult(intent, ALBUM_OK)
             }
 
         })
 
         view.iv_picture.setOnClickListener {
-
+            permissionCheck.hasPermissions(arrayListOf(PERMISSION_CAMERA))
         }
 
     }
@@ -47,5 +63,10 @@ class PictureFragment : Fragment() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         permissionCheck.onRequestPermissionResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.e(TAG, "Fragment: $requestCode, $resultCode, $data")
     }
 }
