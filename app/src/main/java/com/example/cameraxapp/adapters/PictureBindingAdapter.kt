60257dc3b297d9acd8eecf83cb8b941b94074747
DESKTOP-D6FEIOL\wkdgm <sj90947@fixelsoft.com>
2021-04-26 17:05:58 +0900

    [ADD] Picture 바인딩 추가

---
+++ app/src/main/java/com/example/cameraxapp/adapters/PictureBindingAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.cameraxapp.adapters
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.cameraxapp.R
+
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()){
+        Glide.with(view.context)
+            .load(imageUrl)
+            .centerCrop()
+            .into(view)
+    } else {
+        Glide.with(view.context)
+            .load(R.mipmap.ic_launcher)
+            .centerCrop()
+            .into(view)
+    }
+}
