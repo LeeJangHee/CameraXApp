b4d2cbc3bceb9717e32789dffcd3178f90183023
DESKTOP-D6FEIOL\wkdgm <sj90947@fixelsoft.com>
2021-05-14 16:46:32 +0900

    [수정용] 리사이클러뷰 데코레이션 중..

--- app/src/main/java/com/example/cameraxapp/ui/fragment/AlbumFragment.kt
+++ app/src/main/java/com/example/cameraxapp/ui/fragment/AlbumFragment.kt
@@ -1,17 +1,21 @@
 package com.example.cameraxapp.ui.fragment
 
+import android.graphics.Canvas
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cameraxapp.R
 import com.example.cameraxapp.adapters.AlbumAdapter
 import com.example.cameraxapp.databinding.FragmentAlbumBinding
+import com.example.cameraxapp.util.AlbumSelectItemDecoration
 import com.example.cameraxapp.viewmodel.PictureViewModel
 import kotlinx.coroutines.launch
 
@@ -33,26 +37,25 @@ class AlbumFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val customDecoration = AlbumSelectItemDecoration(requireContext())
         binding.apply {
             viewModel = pictureViewModel
             lifecycleOwner = viewLifecycleOwner
             albumRecyclerView.adapter = albumAdapter
-            albumRecyclerView.layoutManager = GridLayoutManager(requireContext(), 4).apply {
-//                reverseLayout = true // RTL 맞춰서 오른쪽으로 쌓임
-//                stackFromEnd = true // 영역 끝부분 부터 추가
-            }
-            albumRecyclerView.addItemDecoration(object: RecyclerView.ItemDecoration() {
-                override fun getItemOffsets(
-                    outRect: Rect,
-                    view: View,
-                    parent: RecyclerView,
-                    state: RecyclerView.State
-                ) {
-                    super.getItemOffsets(outRect, view, parent, state)
-                    val position = parent.getChildAdapterPosition(view)
-                    outRect.top = -100
-                }
-            })
+            albumRecyclerView.layoutManager = GridLayoutManager(requireContext(), 4)
+//            albumRecyclerView.addItemDecoration(object: RecyclerView.ItemDecoration() {
+//                override fun getItemOffsets(
+//                    outRect: Rect,
+//                    view: View,
+//                    parent: RecyclerView,
+//                    state: RecyclerView.State
+//                ) {
+//                    super.getItemOffsets(outRect, view, parent, state)
+//                    val position = parent.getChildAdapterPosition(view)
+//                    outRect.top = -100
+//                }
+//            })
+            albumRecyclerView.addItemDecoration(customDecoration)
         }
 
         lifecycleScope.launch {
---
+++ app/src/main/java/com/example/cameraxapp/util/AlbumSelectItemDecoration.kt
@@ -0,0 +1,72 @@
+package com.example.cameraxapp.util
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import android.widget.TextView
+import androidx.compose.ui.graphics.Paint
+import androidx.recyclerview.widget.RecyclerView
+
+class AlbumSelectItemDecoration(private val context: Context) : RecyclerView.ItemDecoration() {
+
+    private val paint = Paint()
+
+    init {
+        paint.color = androidx.compose.ui.graphics.Color.Transparent
+    }
+
+    fun drawView(c: Canvas, v: View) {
+        c.save()
+        v.draw(c)
+    }
+
+    fun dpToPx(context: Context, dp: Float): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val position = parent.getChildAdapterPosition(view)
+
+
+
+        outRect.top = dpToPx(context, 10f).toInt()
+        outRect.bottom = dpToPx(context, 10f).toInt()
+    }
+
+
+//    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+//
+//        super.onDrawOver(c, parent, state)
+//        val itemCount = parent.childCount
+//        for (i in 0 until itemCount) {
+//            val child = parent.getChildAt(i)
+//
+//            val params = child.layoutParams as RecyclerView.LayoutParams
+//
+//            val cTop = child.top
+//            val cRight = child.right
+//            val cBottom = child.bottom
+//            val cLeft = child.left
+//
+//
+//            val textView = TextView(parent.context)
+//            textView.text = "kejfaiwejfjdkafjiwjeifji"
+//            textView.setBackgroundColor(Color.RED)
+//            textView.layout(cLeft, cTop, cRight, cBottom)
+//
+//            c.translate(cLeft.toFloat(), cTop.toFloat())
+//
+//            textView.draw(c)
+////            drawView(c, textView)
+//        }
+//    }
+}
