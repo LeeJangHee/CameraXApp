210c1c66d1821ff4b2be503a93f940f1102e366f
DESKTOP-D6FEIOL\wkdgm <sj90947@fixelsoft.com>
2021-05-14 16:08:33 +0900

    [FIX] 버튼 1개만 나오게하기

--- app/src/main/java/com/example/cameraxapp/adapters/AlbumAdapter.kt
+++ app/src/main/java/com/example/cameraxapp/adapters/AlbumAdapter.kt
@@ -1,42 +1,34 @@
 package com.example.cameraxapp.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.get
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cameraxapp.databinding.ItemAlbumBinding
 import com.example.cameraxapp.model.PictureModel
-import com.example.cameraxapp.util.AlbumDiffUtil
 
 const val TAG = "janghee"
 
 class AlbumAdapter : RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
 
     private var pictureList = List<PictureModel?>(8) { null } as ArrayList<PictureModel?>
-    private var buttonView = ArrayList<Boolean>()
-    private var selectItemIndex = -1
+    private var buttonViewList = ArrayList<Boolean>()
+    private var selectItemIndex = -1 // -1 : not selected  / 0: first item
 
 
     inner class AlbumViewHolder(
         val binding: ItemAlbumBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-//        init {
-//            clickListener = AlbumListener {
-//                Toast.makeText(binding.root.context, "click $it", Toast.LENGTH_SHORT).show()
-//                binding.bool = it
-//            }
-//        }
-//
-//        fun bind(pictureModel: PictureModel, isBool: Boolean) {
-//            with(binding) {
-//                result = pictureModel
-//                executePendingBindings()
-//            }
-//        }
+        init {
+
+        }
+
+        fun bind(pictureModel: PictureModel, isBool: Boolean) {
+            with(binding) {
+                result = pictureModel
+                executePendingBindings()
+            }
+        }
 
     }
 
@@ -51,55 +43,34 @@ class AlbumAdapter : RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-        buttonView.add(false)
-        val currentPicture = pictureList[position]
+        buttonViewList.add(false)
 
-//        pictureList[position]?.let { holder.bind(it, buttonView[position]) }
+        // 버튼 visible / gone
+        holder.binding.bool = buttonViewList[position]
 
-        holder.binding.apply {
-            bool = this@AlbumAdapter.buttonView[position]
-            click = AlbumListener {
-                val preIndex = selectItemIndex
-                selectItemIndex = position
-
-                this@AlbumAdapter.buttonView[preIndex] = !this@AlbumAdapter.buttonView[preIndex]
-                this@AlbumAdapter.buttonView[position] = !this@AlbumAdapter.buttonView[position]
-            }
-            result = currentPicture
-        }
+        // 리사이클러 아이템 클릭
         holder.itemView.setOnClickListener {
             val preIndex = selectItemIndex
             selectItemIndex = position
 
-            if (preIndex < 0) {
-                this@AlbumAdapter.buttonView[position] = !this@AlbumAdapter.buttonView[position]
+            if (preIndex == -1) {
+                buttonViewList[position] = !buttonViewList[position]
                 notifyItemChanged(position)
             } else {
                 if (preIndex == position) {
-                    this@AlbumAdapter.buttonView[preIndex] = !this@AlbumAdapter.buttonView[preIndex]
+                    buttonViewList[position] = !buttonViewList[position]
+                    notifyItemChanged(position)
+                    selectItemIndex = -1
+                } else {
+                    buttonViewList[position] = !buttonViewList[position]
+                    buttonViewList[preIndex] = !buttonViewList[preIndex]
+
                     notifyItemChanged(preIndex)
-                    return@setOnClickListener
+                    notifyItemChanged(position)
                 }
-                this@AlbumAdapter.buttonView[preIndex] = !this@AlbumAdapter.buttonView[preIndex]
-                this@AlbumAdapter.buttonView[position] = !this@AlbumAdapter.buttonView[position]
-
-                notifyItemChanged(preIndex)
-                notifyItemChanged(position)
             }
-
         }
-//        holder.itemView.setOnClickListener {
-//            val preIndex = selectItemIndex
-//            selectItemIndex = position
-//
-//            buttonView[preIndex] = !buttonView[preIndex]
-//            buttonView[position] = !buttonView[position]
-//
-//            notifyItemChanged(preIndex)
-//            notifyItemChanged(position)
-//
-//            clickListener.onItemClick()
-//        }
+        pictureList[position]?.let { holder.bind(it, buttonViewList[position]) }
     }
 
     override fun getItemCount(): Int {
@@ -109,13 +80,5 @@ class AlbumAdapter : RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
     fun setData(newList: ArrayList<PictureModel?>) {
         pictureList = newList
         notifyDataSetChanged()
-//        val albumDiffUtil = AlbumDiffUtil(oldList = pictureList, newList = newList)
-//        val diffUtilResult = DiffUtil.calculateDiff(albumDiffUtil)
-//        pictureList = newList
-//        diffUtilResult.dispatchUpdatesTo(this)
-    }
-
-    fun interface AlbumListener {
-        fun onItemClick(isVisible: Boolean)
     }
 }
